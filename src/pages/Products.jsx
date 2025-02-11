30d8ad729c5a396e1d35e91b5afbd946c7e9a1b9
Alphaboy20023 <timivictor565@gmail.com>
2025-02-11 19:43:11 +0100

    fix product.slice issue

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -237,7 +237,7 @@ function Home(params) {
                 </tr>
               </thead>
               <tbody>
-                {filteredStores.map((product, index) =>
+                {filteredStores && filteredResults.map((product, index) =>
                   <tr key={product.id}>
                     <td>{index + 1}</td>
                     <td>{product.title}</td>
